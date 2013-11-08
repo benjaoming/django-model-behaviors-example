749dd8af6533cc9db1e8b6946a86acc9bc898914
benjaoming <benjaoming@gmail.com>
2013-11-08 19:06:10 +0100

    Add support for chained .none()
    You need this to have your querysets fully identical to standard queryset behavior :) Notice there's some change in Django 1.6 that I didn't fully catch up on.

--- example/updated/querysets.py
+++ example/updated/querysets.py
@@ -6,7 +6,14 @@ class PublishableQuerySet(models.query.QuerySet):
         from django.utils import timezone
         return self.filter(~models.Q(publish_date=None)).filter(publish_date__lte=timezone.now())
 
+class PublishableEmptyQuerySet(models.query.EmptyQuerySet):
+    def published(self):
+        return self
 
 class AuthorableQuerySet(models.query.QuerySet):
     def authored_by(self, author):
         return self.filter(author__username=author)
+
+class AuthorableEmptyQuerySet(models.query.EmptyQuerySet):
+    def authored_by(self, author):
+        return self
